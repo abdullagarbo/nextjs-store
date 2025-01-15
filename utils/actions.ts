26fc9cb33442a0ae18dc150820659d863869ab53
Abdulla <abdulla.garbo@gmail.com>
2025-01-15 16:25:09 +0100

    bug fix

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,7 +3,7 @@
 import { redirect } from 'next/navigation';
 import db from '@/utils/db';
 
-import { auth, currentUser } from '@clerk/nextjs/server';
+import { currentUser } from '@clerk/nextjs/server';
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
@@ -68,7 +68,7 @@ const getAuthUser = async () => {
 };
 
 export const createProductAction = async (
-  prevState: any,
+  prevState: unknown,
   formData: FormData
 ): Promise<{ message: string }> => {
   const user = await getAuthUser();
@@ -77,7 +77,7 @@ export const createProductAction = async (
     const name = formData.get('name') as string;
     const company = formData.get('company') as string;
     const price = Number(formData.get('price') as string);
-    const image = formData.get('image') as File;
+    // const image = formData.get('image') as File;
     const description = formData.get('description') as string;
     const featured = Boolean(formData.get('featured') as string);
 
