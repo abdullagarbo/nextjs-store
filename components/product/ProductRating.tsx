e1d89d7bfcef9e0b7894d5f4e9559177d32f6024
Abdulla <abdulla.garbo@gmail.com>
2025-01-15 16:19:58 +0100

    bug fix

--- components/product/ProductRating.tsx
+++ components/product/ProductRating.tsx
@@ -6,6 +6,7 @@ async function ProductRating({ productId }: { productId: string }) {
 
   const className = `flex gap-1 items-center text-md mt-1 mb-4`;
   const countValue = `(${count}) reviews`;
+  console.log(productId);
   return (
     <span className={className}>
       <FaStar className='w-3 h-3' />
